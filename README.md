2a8e42d3ce8f12c4a686f10251ca640edd924ac7
Mark Henwood <mcbhenwood@icloud.com>
2023-02-01 09:46:05 +0000

    Add a method which consumes another service

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # dummy-flask-app
 
 Simple 'Hello World' style app to give something to deploy.
+
+## Build
+
+```bash
+docker build -t dummy .
+```
+
+### Run
+
+```bash
+docker run -p "8080:80" -e "FOREIGN_SERVICE_URL=SOMEURL" dummy
+```
+
+Go to <http://localhost:8080> in your browser.
+
+To access the URL which consumes some other service, be sure to specify a valid `FOREIGN_SERVICE_URL` as per the `docker run` instruction above, then go in your browser to <http://localhost:8080/retrieve_other_service_content>.
--- dummy/__init__.py
+++ dummy/__init__.py
@@ -1,7 +1,20 @@
+import os
+
+import requests
 from flask import Flask, Response
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return Response("Hello! (version 2)")
+
+
+@app.route("/retrieve_other_service_content")
+def retrieve_other_service_content():
+    foreign_service_url = os.getenv("FOREIGN_SERVICE_URL", None)
+    if not foreign_service_url:
+        return Response("Service is not configure with FOREIGN_SERVICE_URL :(")
+    r = requests.get(foreign_service_url)
+    return Response(' Foreign service said "{}"'.format(r.text))
